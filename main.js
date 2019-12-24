820159b53bac7167916807eb6ea8b3349eb1e888
limoney <szakal2289@gmail.com>
2019-12-24 13:25:01 +0100

    color kinda works

--- main.js
+++ main.js
@@ -1,51 +1,54 @@
 let r = 100;
 let circlePath = [];
 let rectPath = [];
+let trianglePath = []
+let currentShape = [];
+let hexagonPath = [];
 let res = 10;
+let desiredShapeArray = [];
+let lastShape = [];
+let reverse = false;
+let defaultColor;
+let desiredColor;
+let tmp = 0;
 function setup()
 {
   createCanvas(400,400);
   angleMode(DEGREES);
   ellipseMode(CENTER);
   r=150;
-  for(let i=-45;i<45;i+=res)
+  defaultColor = color(255,0,0);
+  desiredColor = color(255,255,0);
+  for(let i=-45;i<315;i+=res)
   {
     circlePath.push(createVector(cos(i)*r,sin(i)*r));
+    currentShape.push(createVector(cos(i)*r,sin(i)*r));
+    desiredShapeArray.push(createVector(cos(i)*r,sin(i)*r));
+    lastShape.push(createVector(cos(i)*r,sin(i)*r));
   }
-  r=75;
+
   rectPath.push(createVector(cos(45)*r,sin(-45)*r));
   rectPath.push(createVector(cos(45)*r,sin(45)*r));
-  //rectPath.push(createVector(cos(135)*r,sin(45)*r));
-  //rectPath.push(createVector(cos(135)*r,sin(-45)*r));
+  rectPath.push(createVector(cos(135)*r,sin(45)*r));
+  rectPath.push(createVector(cos(135)*r,sin(-45)*r));
 
-  let rectPathcp = rectPath.slice();
-  console.log("sb "+ rectPathcp.length);
-  for(let i = 0; i< rectPath.length;i++)
-  {
-    let injectPoints = [];
-    let current = rectPath[i].copy();
-    let next = rectPath[(i+1)%rectPath.length].copy();
-    const distance = dist(current.x,current.y,next.x,next.y);
-    const dir = next.sub(current).normalize();
-    console.log(distance);
-    //let jump = distance/((circlePath.length-4))*2;
-    let jump = distance/(32)*2;
-    //for(let j = 1;j<circlePath.length/4;j++)
-    for(let j = 1;j<36/8;j++)
-    {
-      let distcp = dir.copy();
-      let sth = createVector(current.x,current.y).add(distcp.add(distcp.mult(j*jump)));
-      injectPoints.push(sth);
-      console.log("call");
-    }
-    //jak to nie zadzialalo jak chcialem to rip z debugowanie tego w przyszlosci
-    //rectPath.splice(injectPoints.length,0,...injectPoints);
-    console.log("si "+ injectPoints.length);
-    rectPathcp.insert2(rectPathcp.indexOf(current),injectPoints);
+  rectPath = createPath(rectPath);
 
-  }
-  rectPath = rectPathcp;
-  console.log("se "+ rectPath.length);
+  trianglePath.push(createVector(0,sin(-45)*r));
+  trianglePath.push(createVector(cos(45)*r,sin(45)*r));
+  trianglePath.push(createVector(cos(135)*r,sin(45)*r));
+
+  trianglePath = createPath(trianglePath);
+
+  r*=0.75;
+  hexagonPath.push(createVector(cos(0)*r,sin(0)*r));
+  hexagonPath.push(createVector(cos(60)*r,sin(60)*r));
+  hexagonPath.push(createVector(cos(120)*r,sin(120)*r));
+  hexagonPath.push(createVector(cos(180)*r,sin(0)*r));
+  hexagonPath.push(createVector(cos(240)*r,sin(240)*r));
+  hexagonPath.push(createVector(cos(300)*r,sin(300)*r));
+
+  hexagonPath = createPath(hexagonPath);
 }
 
 function draw()
@@ -53,35 +56,109 @@ function draw()
   translate(width/2,height/2);
   background(51);
   strokeWeight(4);
-
-  for(let i=0;i<circlePath.length;i++)
-  {
-    i%2==0 ? stroke(255,0,0,128) : stroke(255,128,255,64);
-    let vec = rectPath[i].copy().sub(circlePath[i]);
-    line(circlePath[i].x,circlePath[i].y,rectPath[i].x,rectPath[i].y);
-    vec.normalize();
-    //circlePath[i].add(vec);
-  }
   noFill();
-  stroke(100,150,200);
+  updateVertices();
+
+
+  let c = currentShape[0].copy();
+  let d = desiredShapeArray[0].copy();
+  let l = lastShape[0].copy();
+
+  let fulldist = dist(d.x,d.y,l.x,l.y);
+  let lastdist = dist(l.x,l.y,c.x,c.y);
+  let currdist = dist(d.x,d.y,c.x,c.y);
+  tmp = currdist/fulldist;
+  //console.log(fulldist+" "+lastdist+" "+currdist);
+
+
+  stroke(lerpColor(defaultColor,desiredColor,tmp));
   beginShape();
-  for(let point of circlePath)
+  for(let point of currentShape)
   {
-    ellipse(point.x,point.y,10);
-    //vertex(point.x,point.y);
+    //ellipse(point.x,point.y,5);
+    vertex(point.x,point.y);
   }
   endShape(CLOSE);
-  stroke(200,150,100,64);
-  for(let point of rectPath)
+  for(let point of lastShape)
   {
-    ellipse(point.x,point.y,10);
+    //ellipse(point.x,point.y,10);
   }
+  if(tmp>1)
+  {
+    defaultColor = swap(defaultColor,defaultColor=desiredColor);
+    desiredColor = swap(defaultColor,defaultColor=desiredColor);
+    tmp =0;
+    console.log("swap");
+  }
+
 }
 
-Array.prototype.insert2 = function(index) {
+Array.prototype.insert = function(index) {
     index = Math.min(index+1, this.length);
     arguments.length > 1
         && this.splice.apply(this, [index, 0].concat([].pop.call(arguments)))
-        && this.insert2.apply(this, arguments);
+        && this.insert.apply(this, arguments);
     return this;
 };
+
+function createPath(vertices)
+{
+  let verticesCopy = vertices.slice();
+  for(let i=0;i<vertices.length;i++)
+  {
+    let current = vertices[i].copy();
+    let next = vertices[(i+1)%vertices.length].copy();
+    let distance = dist(current.x,current.y,next.x,next.y);
+    let dir = next.copy().sub(current).normalize();
+    let segmentCount = circlePath.length/vertices.length;
+    let segments = [];
+    let jump = distance/segmentCount;
+    for(let j=1;j<segmentCount;j++)
+    {
+      let nextstep = dir.copy().mult(jump*j);
+      segments.push(current.copy().add(nextstep));
+    }
+    verticesCopy.insert(i*segmentCount,segments);
+  }
+  return verticesCopy;
+}
+function updateVertices()
+{
+  let finished = true;
+  for(let i=0;i<circlePath.length;i++)
+  {
+    let c = currentShape[i].copy();
+    let d = desiredShapeArray[i].copy();
+    let distance = dist(c.x,c.y,d.x,d.y);
+    if(distance>1) finished = false;
+    i%2 ? stroke(150,100,200,64) : stroke(200,100,150,64);
+    //line(c.x,c.y,d.x,d.y);
+    let vec;
+    vec = d.sub(c).setMag(distance*0.01);
+    currentShape[i].add(vec);
+  }
+  if(finished)
+  {
+    if(reverse)
+    {
+      lastShape = desiredShapeArray.slice();
+      setTimeout(()=>
+      {
+        desiredShapeArray = circlePath.slice();
+        reverse = false;
+      },31);
+    }
+    else
+    {
+      lastShape = desiredShapeArray.slice();
+      setTimeout(()=>
+      {
+        desiredShapeArray = hexagonPath.slice();
+        reverse = true;
+      },31);
+    }
+
+  }
+}
+
+function swap(x){return x}
